4c688655739d9d899cacc3d0086b5dd2276461d5
Gregory Cabral De Los Santos <Blackarcanjus@gmail.com>
2026-02-26 17:36:48 -0400

    Added profile signing

--- lib/services/scrapers/hosters/datanodes_hoster.dart
+++ lib/services/scrapers/hosters/datanodes_hoster.dart
@@ -72,7 +72,7 @@ class DatanodesHoster implements Hoster {
         'method_free': 'Free Download >>',
         'method_premium': '',
         '__dl': '1',
-        'g_captch__a': '1', // <-- NUEVO (clave)
+        'g_captch__a': '1',
       };
 
       // -------------------------
